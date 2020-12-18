ba0e13898bc8ef56886a1ee4476089fb0059ab42
Peter Stensmyr <peter.stensmyr@iress.com>
2020-12-18 16:17:13 +1100

    Add .gitconfig and config.fish

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,7 @@
+abbr cd.. ".."
+abbr cd... "../.."
+abbr cd.... "../../.."
+abbr cd..... "../../../.."
+abbr gitb "git checkout --track origin/master -b"
+eval (python -m virtualfish)
+fish_vi_key_bindings
