78e7282d3f6ac5d1881ac559efd85b53569182d4
Peter Stensmyr <peter.stensmyr@gmail.com>
2021-04-21 14:38:17 +1000

    Add pyenv and l

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,8 +2,16 @@ abbr cd.. ".."
 abbr cd... "../.."
 abbr cd.... "../../.."
 abbr cd..... "../../../.."
+abbr l "ls -la"
 abbr gitb "git checkout --track origin/master -b"
 fish_vi_key_bindings
 
 status --is-interactive; and rbenv init - | source
+
+
+# pyenv init
+if command -v pyenv 1>/dev/null 2>&1
+  pyenv init - | source
+end
+
 starship init fish | source
